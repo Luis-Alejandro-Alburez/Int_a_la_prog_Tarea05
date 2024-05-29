683754eda56eb1e8a03849f4ec0c8cc8075c1122
Luis Alburez <alburezluis@gmail.com>
2024-05-29 12:19:35 -0600

    agregando problema 1 a la tarea

---
+++ problema1.js
@@ -0,0 +1,55 @@
+//usuario2
+let nombre2 = 'Luis';
+let apellido2 = 'Alburez';
+let email2 = 'alburezluis@galileo.edu';
+let direccion2 = {
+    municipio: 'Antigua',
+    calle: '4ta calle',
+    numero: 21
+};
+
+//usuario3
+let nombre3 = 'Juan';
+let apellido3 = 'Alburez';
+let email3 = 'alburezjuan@galileo.edu';
+let direccion3 = {
+    municipio: 'Antigua',
+    calle: '4ta calle',
+    numero: 12
+};
+
+//usuario4
+let nombre4 = 'Rosa';
+let apellido4 = 'Samayoa';
+let email4 = 'rosayoa22@galileo.edu';
+let direccion4 = {
+    municipio: 'Antigua',
+    calle: '4ta calle',
+    numero: 6
+};
+
+//usuario5
+let nombre5 = 'Rosa';
+let apellido5 = 'Corado';
+let email5 = 'coradorosa@galileo.edu';
+let direccion5 = {
+    municipio: 'Antigua',
+    calle: '5ta calle',
+    numero: 18
+}
+
+//usuario6
+let nombre6 = 'Ana';
+let apellido6 = 'Hurtarte';
+let email6 = 'hurtarteana@galileo.edu';
+let direccion6 = {
+    municipio: 'Jocotenango',
+    calle: 'Calle real',
+    numero: 20
+};
+
+//¿Puedes mostrar estas características de los usuarios de manera ordenada y coherente en consola?
+//R: No, tendría que acordarme cuál de los números corresponde para cada usuario y no es como que tengan un índice o algo así, es un desorden trabajar con esta estructura.
+
+//¿Tienen los usuarios alguna funcionalidad, pueden hacer algo?
+//R: No, los usuarios no tienen ninguna función asociada, simplemente estamos recopilando información respecto a los mismos.
